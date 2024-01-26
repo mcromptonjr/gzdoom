2a57d37a847d7697ef53bfe0dcf3f3a58eff95c5
unknown <mcromptonjr@gmail.com>
2024-01-25 22:30:15 -0800

    Signed-off-by: unknown <mcromptonjr@gmail.com>

---
+++ Join.bat
@@ -0,0 +1,2 @@
+gzdoom -file Avactor.wad SmoothDoomLE-20230822.pk3 antiblock.wad aspot-v1.1.zip zdoom_coop_utils_v1.pk7 -join 174.62.74.252 -port 15777
+gzdoom doom.wad -join 25.62.167.255 -port 15777 -file SIGIL_II_V1_0.WAD SmoothDoomLE-20230822.pk3 antiblock.wad aspot-v1.1.zip zdoom_coop_utils_v1.pk7
